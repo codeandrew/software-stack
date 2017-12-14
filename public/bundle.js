7229c3fa80fa08606b3a6230d4691ce6aa5d6b03
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-14 18:39:55 +0800

    Create BookItem Component

--- public/bundle.js
+++ public/bundle.js
@@ -19547,6 +19547,10 @@ var _booksActions = __webpack_require__(75);
 
 var _reactBootstrap = __webpack_require__(193);
 
+var _bookItem = __webpack_require__(333);
+
+var _bookItem2 = _interopRequireDefault(_bookItem);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19576,28 +19580,14 @@ var BookList = function (_React$Component) {
       console.log(" this.props.books :::", this.props.books);
       var booksList = this.props.books.map(function (booksArr) {
         return _react2.default.createElement(
-          'div',
-          { key: booksArr.id },
-          _react2.default.createElement(
-            'h2',
-            null,
-            booksArr.title
-          ),
-          _react2.default.createElement(
-            'h3',
-            null,
-            booksArr.description
-          ),
-          _react2.default.createElement(
-            'h3',
-            null,
-            booksArr.price
-          ),
-          _react2.default.createElement(
-            _reactBootstrap.Button,
-            { bsStyle: 'primary' },
-            'Buy Now'
-          )
+          _reactBootstrap.Col,
+          { xs: 12, sm: 6, md: 4, key: booksArr.id },
+          _react2.default.createElement(_bookItem2.default, {
+            id: booksArr.id,
+            title: booksArr.title,
+            description: booksArr.description,
+            price: booksArr.price
+          })
         );
       });
       console.log(booksList);
@@ -40428,6 +40418,87 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
+
+/***/ }),
+/* 333 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(32);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactBootstrap = __webpack_require__(193);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var BookItem = function (_React$Component) {
+  _inherits(BookItem, _React$Component);
+
+  function BookItem() {
+    _classCallCheck(this, BookItem);
+
+    return _possibleConstructorReturn(this, (BookItem.__proto__ || Object.getPrototypeOf(BookItem)).apply(this, arguments));
+  }
+
+  _createClass(BookItem, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        _reactBootstrap.Well,
+        { bsSize: 'large' },
+        _react2.default.createElement(
+          _reactBootstrap.Row,
+          null,
+          _react2.default.createElement(
+            _reactBootstrap.Col,
+            { xs: 12 },
+            _react2.default.createElement(
+              'h6',
+              null,
+              this.props.title
+            ),
+            _react2.default.createElement(
+              'p',
+              null,
+              ' ',
+              this.props.description
+            ),
+            _react2.default.createElement(
+              'h6',
+              null,
+              ' usd. ',
+              this.props.price
+            ),
+            _react2.default.createElement(
+              _reactBootstrap.Button,
+              { bsStyle: 'primary' },
+              'Buy Now'
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return BookItem;
+}(_react2.default.Component);
+
+exports.default = BookItem;
 
 /***/ })
 /******/ ]);
