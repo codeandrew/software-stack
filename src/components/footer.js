ca19f7d25be76be4d6245c38ff3a218961a5ace6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-19 15:52:38 +0800

    add the navbar and footer

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+"use strict"
+import React from 'react'
+
+class Footer extends React.Component{
+  render(){
+    return (
+      <footer className="footer text-center">
+        <div className="container">
+          <p className="footer-text">
+            copyright 2017 BookShop. All Rights Reserved
+          </p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
