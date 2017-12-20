517912f0692f5b5bb295774d0b8750f9dedb0aed
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-20 11:33:08 +0800

    react-router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { Router, Route, IndexRoute, browserHistory} from 'react-router'
+
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger'
 
@@ -17,17 +19,24 @@ const middleware = applyMiddleware(logger)
 const store = createStore(reducers, middleware);
 
 
-import BooksList from './components/pages/booksList';
-import Menu from './components/menu'
-import Footer from './components/footer'
+import BooksList from './components/pages/booksList'
+import Cart from './components/pages/cart'
+import BooksForm from './components/pages/booksForm'
+import Main from './main'
+
+const Routes = (
+  <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+          <IndexRoute component={BooksList} />
+          <Route path="/admin" component={BooksForm} />
+          <Route path="/cart" component={Cart} />
+        </Route>
+      </Router>
+  </Provider>
+)
 
 render(
-  <Provider store={store}>
-    <div>
-      <Menu />
-      <BooksList />
-      <Footer />
-    </div>
-  </Provider>,
+  Routes,
   document.querySelector('#app')
 );
