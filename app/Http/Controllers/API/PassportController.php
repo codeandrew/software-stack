6af4c89e789441038571b5cbe565cd665e236435
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-02-21 19:31:39 +0800

    added PassportController

---
+++ app/Http/Controllers/API/PassportController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class PassportController extends Controller
+{
+    //
+    public $successStatus = 200;
+
+    public function register(Request $request){
+      //
+      $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
+        'c_password' => 'required|same:password'
+      ]);
+
+      if ($validator->fails()){
+        return response()->json(['error' => $validator->errors(), 401])
+      }
+
+      //success
+      $input = request->all();
+      $input['password'] = bcrypt();// encrypted the password
+      $user = User::create($input)
+      $success['token'] = $user->createToken('WebServiceApp')->accessToken;
+      $success['name'] = $user->name;
+
+      return response()->json(['success'=> $success], $this->$successStatus);
+
+
+    }
+
+    public function login(Request $request){
+
+    }
+}
